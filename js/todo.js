04596154118ac692274d72da7c40c13eafa4d5e3
Hyerin <hyerin.bcit@gmail.com>
2023-01-13 17:43:10 -0800

    Deleting todos

--- js/todo.js
+++ js/todo.js
@@ -9,21 +9,31 @@ let toDos = [];
 function saveToDos(){
   // save the lists to the array
   localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
-  // console.log(localStorage);
 }
 
 function deleteTodo(event){
   // can check who was the clicked
   // console.log(event.target.parentElement.innerText);
   const li = event.target.parentElement;
+  // console.log(li.id);
+  
   li.remove();
+  
+  // remove the li that was clicked
+  // remain the array with elements that are not same as li.id (= clicked todos)
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  // console.log(typeof li.id);        // li.id -> string, but todo.id -> number => need to use parseInt
+  
+  // after delete save again
+  saveToDos();
 }
 
 function paintTodo(newTodo){
   // making list with newTodo value
   const li = document.createElement("li");
+  li.id = newTodo.id;
   const span = document.createElement("span");
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
   
   // create button for delete lists
   const button = document.createElement("button");
@@ -45,11 +55,14 @@ function handleToDoSubmit(event){
   // after enter the value should be empty
   toDoInput.value = "";
   
-  // add the value to the toDos array
-  toDos.push(newTodo);
-
-  // sending the newTodo value to the paintTodo func
-  paintTodo(newTodo);
+  // add the value to the toDos array as a array with object
+  const newTodoObj = {
+    text:newTodo,
+    id: Date.now()       // give us a random number
+  }
+  toDos.push(newTodoObj);
+  // sending the newTodoObj value to the paintTodo func
+  paintTodo(newTodoObj);
 
   // save todos
   saveToDos();
